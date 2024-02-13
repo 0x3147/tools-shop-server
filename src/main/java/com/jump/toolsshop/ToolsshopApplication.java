3b3b751a8dc2289330606adf592f5adaf3406034
radeonvii <kjx01771103@cainiao.com>
2024-02-13 20:28:18 +0800

    Initial commit

---
+++ src/main/java/com/jump/toolsshop/ToolsshopApplication.java
@@ -0,0 +1,13 @@
+package com.jump.toolsshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToolsshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToolsshopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jump/toolsshop/ToolsshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jump.toolsshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolsshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
