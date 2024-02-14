0d32f4e0aadb243acf98b0951b0dee4db202dd4a
radeonvii <kjx01771103@cainiao.com>
2024-02-14 20:06:19 +0800

    feat: 注册功能初步

--- src/main/java/com/jump/toolsshop/service/UserService.java
+++ src/main/java/com/jump/toolsshop/service/UserService.java
@@ -1,5 +1,7 @@
 package com.jump.toolsshop.service;
 
+import com.jump.toolsshop.exception.ToolsShopErrorEnum;
+import com.jump.toolsshop.exception.ToolsShopException;
 import com.jump.toolsshop.mapper.UserMapper;
 import org.springframework.stereotype.Service;
 
@@ -10,11 +12,11 @@ public class UserService {
     @Resource
     UserMapper userMapper;
 
-    public void register(String username, String email, String password) {
+    public void register(String username, String email, String password) throws Exception {
         var res = userMapper.selectByUserName(username);
 
         if (res != null) {
-
+            throw new ToolsShopException(ToolsShopErrorEnum.USER_NAME_EXIST);
         }
     }
 }
