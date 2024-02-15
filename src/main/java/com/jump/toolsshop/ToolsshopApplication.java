d0c75568aae0cce1a92a199883017319e0ac58b9
radeonvii <kjx01771103@cainiao.com>
2024-02-15 14:28:43 +0800

    feat: 改回logback配置

--- src/main/java/com/jump/toolsshop/ToolsshopApplication.java
+++ src/main/java/com/jump/toolsshop/ToolsshopApplication.java
@@ -1,6 +1,8 @@
 package com.jump.toolsshop;
 
 import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,8 +10,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @MapperScan("com.jump.toolsshop.mapper")
 public class ToolsshopApplication {
 
+    private static final Logger LOG = LoggerFactory.getLogger(ToolsshopApplication.class);
+
     public static void main(String[] args) {
-        SpringApplication.run(ToolsshopApplication.class, args);
+
+        var app = SpringApplication.run(ToolsshopApplication.class, args);
+
+        var port = app.getEnvironment().getProperty("server.port");
+
+        LOG.info("服务已启动!");
+
+        LOG.info("服务地址: \thttp://127.0.0.1:{}", port);
     }
 
 }
